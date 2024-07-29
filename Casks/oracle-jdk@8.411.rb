fdc073683d884c3a3ad08ab37268f3f7e3d86c30
Alexander Sechejko <sechejko@gmail.com>
2024-07-29 02:43:09 +0200

    fix(oracle-jdk@8.411): update sha256

--- Casks/oracle-jdk@8.411.rb
+++ Casks/oracle-jdk@8.411.rb
@@ -1,9 +1,9 @@
-#noinspection RubyResolve,SpellCheckingInspection
+# noinspection RubyResolve,SpellCheckingInspection
 cask "oracle-jdk@8.411" do
   arch arm: "aarch64"
 
   version "8u411"
-  sha256 arm:   "d4d3e4ced023c7717e81c05d372e37e3787a2c1c17847a68210a95c7a9367214"
+  sha256 arm: "03353ce3f8bd37488d116b29ede7ec82af05a82524868f4181be67d8c748c5f6"
 
   url "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249841_43d62d619be4e416215729597d70b8ac"
   name "Oracle Java Standard Edition Development Kit"
