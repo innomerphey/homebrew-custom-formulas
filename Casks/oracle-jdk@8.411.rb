1a2d258fd5f08af6ff2638847dfb386feddb3624
Alexander Sechejko <sechejko@gmail.com>
2024-07-29 02:47:32 +0200

    fix(oracle-jdk@8.411): update the pkg file path to match the actual file

--- Casks/oracle-jdk@8.411.rb
+++ Casks/oracle-jdk@8.411.rb
@@ -1,8 +1,7 @@
 # noinspection RubyResolve,SpellCheckingInspection
 cask "oracle-jdk@8.411" do
   arch arm: "aarch64"
-
-  version "8u411"
+  
   sha256 arm: "03353ce3f8bd37488d116b29ede7ec82af05a82524868f4181be67d8c748c5f6"
 
   url "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249841_43d62d619be4e416215729597d70b8ac"
@@ -17,7 +16,7 @@ cask "oracle-jdk@8.411" do
 
   depends_on macos: ">= :mojave"
 
-  pkg "JDK #{version}.pkg"
+  pkg "Java 8 Update 411.app/Contents/Resources/JavaAppletPlugin.pkg"
 
   uninstall pkgutil: "com.oracle.jdk-#{version}"
 
